f9ce33d9cd102b4d89fe388848e0a44f5a41c215
pydevbg <codeonpythonforreal@gmail.com>
2024-04-12 21:58:59 +0300

    add tag model

--- sharemypic/pics/models.py
+++ sharemypic/pics/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    tagged_images = models.ManyToManyField('Image', related_name='image_tags', blank=True)
+
 class Image(models.Model):
     MAX_TITLE_LENGTH = 100
 
@@ -14,6 +18,10 @@ class Image(models.Model):
     generation_description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+    tagged_images = models.ManyToManyField(Tag, related_name='tagged', blank=True)
+
 
     def can_edit(self, user):
         return user == self.user
+
+
--- sharemypic/pics/views.py
+++ sharemypic/pics/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponseForbidden
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views import generic as views
-from sharemypic.pics.models import Image
+from sharemypic.pics.models import Image, Tag
+from django import forms
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image_file', 'title', 'generation_description', 'tagged_images']
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
 
 
 # Create your views here.
@@ -12,34 +23,66 @@ class ImageCreateView(views.CreateView):
     template_name = 'pics/create_pic.html'
     success_url = reverse_lazy('index')
 
-
-    # Associate Image with User
     def form_valid(self, form):
+        form.instance.user = self.request.user
+        image = form.save()
 
-        form.instance.user = self.request.user  # Associate image with current user
-        if 'image' in self.request.FILES:
-            form.instance.image = self.request.FILES['image']
+        tag_form = TagForm(self.request.POST)
+        if tag_form.is_valid():
+            tag_names = tag_form.cleaned_data['name'].split(',')
+            for tag_name in tag_names:
+                tag_name = tag_name.strip()
+                if tag_name:
+                    tag, created = Tag.objects.get_or_create(name=tag_name)
+                    image.tagged_images.add(tag)
+
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag_form'] = TagForm()
+        return context
+
+        # Process tags
+        # tags_input = form.cleaned_data.get('tags')
+        # if tags_input:
+        #     tags_list = [tag.strip() for tag in tags_input.split(',')]
+        #     for tag_name in tags_list:
+        #         tag, _ = Tags.objects.get_or_create(name=tag_name)
+        #         image_instance.tags.add(tag)
+        #
+        # return redirect(self.success_url)
+
 
 class ImageDetailView(views.DetailView):
     model = Image
     template_name = 'pics/details_pic.html'  # Adjust template name as needed
     context_object_name = 'image'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        image = self.get_object()
+        context['tagged_images'] = image.tagged_images.all()  # Assuming tags is a ManyToManyField in Image model
+        return context
+
+
+
+
 
 class ImageEditView(views.UpdateView):
     model = Image
-    fields = ['image_file', 'title', 'generation_description']
+    fields = ['title', 'image_file','generation_description']
     template_name = 'pics/edit_pic.html'
+
     def get_success_url(self):
         return reverse_lazy('details pic', kwargs={'pk': self.object.pk})
 
-    # limit permission to edit
     def dispatch(self, request, *args, **kwargs):
         self.object = self.get_object()
         if not self.object.can_edit(request.user):
             return HttpResponseForbidden("You do not have permission to edit this image.")
         return super().dispatch(request, *args, **kwargs)
+
 
 
 class ImageDeleteView(views.DeleteView):
